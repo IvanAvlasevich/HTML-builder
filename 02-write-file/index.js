dbe86cebcc3a6615dbb9ac15e85f62da586fdd5f
IvanAvlasevich <avlasevich_ij@yahoo.com>
2023-05-04 20:12:45 +0300

    feat: add new code to index.js in 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,18 @@
+
+const { stdin, stdout, stderr  } = require('process');
+const fs = require('fs');
+const path = require('path');
+
+const txtPath = path.join(__dirname,'text.txt');
+const writeStream = fs.createWriteStream(txtPath);
+
+stdout.write('Have a good day! Please, enter the text...\n');
+stdin.on('data', (data) => {
+  if (data.toString() === 'exit\r\n' || data.toString() === 'exit\n'){
+    process.exit();
+  };
+  writeStream.write(data);
+});
+process.on('exit', code => {
+  stdout.write('Good luck, see you!');
+});
